1c7de1db11b0eabf656c1d781b47ce60d595a98f
Marius <mariushb@gmail.com>
2016-02-27 01:56:56 +0100

    Kort.java V01

---
+++ Oblig2/src/Kort.java
@@ -0,0 +1,67 @@
+import java.util.GregorianCalendar;
+
+public abstract class Kort implements Comparable<Kort>, Cloneable {
+	private GregorianCalendar dateCreated;
+	private String fornavn;
+	private String etternavn;
+	protected int pin;
+	private int kortnummer;
+	private int aksesskode;
+	private boolean sperretKort;
+	private static int antallkort = 100000;
+
+	public Kort(String fornavn, String etternavn, int pin) {
+		super();
+		this.dateCreated = new GregorianCalendar();
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.pin = pin;
+		this.kortnummer = ++antallkort;
+		this.aksesskode = aksesskode;
+		this.sperretKort = false;
+	}
+
+	public String getFornavn() {
+		return fornavn;
+	}
+
+	public void setFornavn(String fornavn) {
+		this.fornavn = fornavn;
+	}
+
+	public String getEtternavn() {
+		return etternavn;
+	}
+
+	public void setEtternavn(String etternavn) {
+		this.etternavn = etternavn;
+	}
+
+	public GregorianCalendar getDateCreated() {
+		return dateCreated;
+	}
+
+	public boolean isSperret() {
+		return sperretKort;
+	}
+
+	public void setSperretKort(boolean sperretKort) {
+		this.sperretKort = sperretKort;
+	}
+
+	public abstract boolean sjekkPIN(int pin);
+
+	
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return "Kort \nOpprettet: " + dateCreated.getTime() + "\nNavn: " + fornavn + " " + etternavn + "\nPin: " + pin
+				+ "\nKortnummer: " + kortnummer + "\nAksesskode: " + aksesskode + "\nSperret: " + sperretKort + "\n\n";
+	}
+
+}
